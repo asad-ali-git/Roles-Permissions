5bd229bb31fcd1b3e240479e922f0faa0ec5c340
Asad <asadali.boson@gmail.com>
2024-03-07 00:50:43 +0500

    Improve seeders and fixed get role issue

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,14 +4,26 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class RoleSeeder extends Seeder
 {
     public function run(): void
     {
-        $superAdminRole = Role::where('name', Role::ROLE_SUPER_ADMIN)->first();
-        if (! $superAdminRole) {
-            Role::factory()->create();
+        try {
+            DB::beginTransaction();
+
+            $superAdminRole = Role::where('name', Role::ROLE_SUPER_ADMIN)->first();
+            if (!$superAdminRole) {
+                Role::factory()->create();
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e);
+            throw $e;
         }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,21 +5,33 @@ namespace Database\Seeders;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        $superAdminRole = Role::where('name', Role::ROLE_SUPER_ADMIN)->first();
-        if ($superAdminRole) {
-            $user = User::where('email', 'admin@gmail.com')->first();
-            if (! $user) {
-                User::factory()->create([
-                    'name' => 'Admin',
-                    'email' => 'admin@gmail.com',
-                    'role_id' => $superAdminRole,
-                ]);
+        try {
+            DB::beginTransaction();
+
+            $superAdminRole = Role::withoutGlobalScopes()->where('name', Role::ROLE_SUPER_ADMIN)->first();
+            if ($superAdminRole) {
+                $user = User::where('email', 'admin@gmail.com')->first();
+                if (!$user) {
+                    User::factory()->create([
+                        'name' => 'Admin',
+                        'email' => 'admin@gmail.com',
+                        'role_id' => $superAdminRole->id,
+                    ]);
+                }
             }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e);
+            throw $e;
         }
     }
 }
